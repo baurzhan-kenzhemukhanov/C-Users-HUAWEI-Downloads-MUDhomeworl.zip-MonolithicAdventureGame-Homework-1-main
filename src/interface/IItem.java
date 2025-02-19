71cfe930f7e0b7f8f3d2c39315e280273bd96d2a
baurzhan-kenzhemukhanov <baurzhan.kenzhemukhanov@narxoz.kz>
2025-02-19 21:06:15 +0500

    Interface enumeration

---
+++ src/interface/IItem.java
@@ -0,0 +1,15 @@
+package interfaces;
+
+public interface IItem extends IEntity {
+
+    void use(ICombatant target);
+
+    IItemType.ItemType getItemType();
+}
+
+enum ItemType {
+    BONES_ITEM,
+    OAKSHIELD,
+    STEEL,
+    SWORD
+}
