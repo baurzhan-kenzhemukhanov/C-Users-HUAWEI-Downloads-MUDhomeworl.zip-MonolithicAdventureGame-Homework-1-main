5fbb21b090325bfde6bb091509682a91232cc526
baurzhan-kenzhemukhanov <baurzhan.kenzhemukhanov@narxoz.kz>
2025-02-19 21:09:50 +0500

    Interface enumeration

---
+++ src/items/Oakshield.java
@@ -0,0 +1,25 @@
+package items;
+
+import interfaces.*;
+
+public class Oakshield implements IItem {
+
+    @Override
+    public String getName() {
+        return "Oakshield";
+    }
+
+    @Override
+    public IEntityType.EntityType getType() {
+        return IEntityType.EntityType.ITEM;
+    }
+
+    @Override
+    public IItemType.ItemType getItemType() {
+        return IItemType.ItemType.OAKSHIELD;
+    }
+
+    @Override
+    public void use(ICombatant target) {
+        int updatedExperience = target.getExperience() + 10;}
+    }
